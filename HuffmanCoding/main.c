843555e712acf3f80d223f4072be8b14d763ca60
PlugNCS <michael.nass@free.fr>
2020-11-02 19:11:00 +0100

    Initial Commit

---
+++ HuffmanCoding/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HuffmanCoding
+//
+//  Created by PlugN on 02/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
