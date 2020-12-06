84331d7e0cf579860b82ce4c6ee76b2311c033c0
Turdyo <63463668+Turdyo@users.noreply.github.com>
2020-12-06 18:53:49 +0100

    Main change to display

--- HuffmanCoding/main.c
+++ HuffmanCoding/main.c
@@ -11,23 +11,28 @@
 #include "OccurencesManager.h"
 
 int main(int argc, const char * argv[]) {
-    
+
     printf("RAW Value of A : %d\n", 'A');
-    
+
     printf("ASCII binary of A : %s\n", get_binary((int)'A'));
-    
+
     printf("\n\n");
-    
+
     char* bin;
     load_binary("Alice.txt", &bin);
     save_output("Output.txt", bin);
-    
+
     printf("Alice.txt : %d caractères\nOutput.txt : %d caractères\n", calculate_size("Alice.txt"), calculate_size("Output.txt"));
-    
+
     Occurences* occ = getOccurences(bin);
-    
+
+    while (occ->next != NULL) {
+        printf("%c %d\n", occ->letter, occ->count);
+        occ = occ->next;
+    }
+
     printf("%s\n", bin);
-    
-    
+
+
     return 0;
 }
