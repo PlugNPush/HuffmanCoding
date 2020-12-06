d1dafcba3efc820c7882a38f78cb9a9602857182
PlugNCS <michael.nass@free.fr>
2020-12-06 17:07:38 +0100

    Partie 1 - Fondations

---
+++ HuffmanCoding/global.h
@@ -0,0 +1,15 @@
+//
+//  glonbal.h
+//  HuffmanCoding
+//
+//  Created by PlugN on 6.12.20..
+//
+
+#ifndef global_h
+#define global_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* global_h */
