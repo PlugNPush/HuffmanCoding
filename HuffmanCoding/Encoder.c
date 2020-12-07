3a0962bad21fc7ef4ff87d26a893ec44d670a66b
PlugNCS <michael.nass@free.fr>
2020-12-07 03:58:24 +0100

    A vérifier

--- HuffmanCoding/Encoder.c
+++ HuffmanCoding/Encoder.c
@@ -14,13 +14,15 @@ char* get_dico(char* location, char character) {
         exit(EXIT_FAILURE);
     }
 
-    int* size;
-    fscanf(dico, "MAX_BITS:%d\n", &size);
-    printf("%d", size);
+    int* size = malloc(sizeof(int));
+    fscanf(dico, "MAX_BITS:%d", size);
+    printf("%d", *size);
 
     char* chaine = (char*)malloc(sizeof(char)*(*size)); //modifier plus tard
-
-    while (fgetc(dico) != character) { //Pour aller au caractere voulu
+    char temp = fgetc(dico);
+    while (temp != character && temp != EOF) {
+        temp = fgetc(dico);
+        //Pour aller au caractere voulu
     }
 
     if (fgetc(dico) != ':') { //Pour les deux points et le retour a la ligne
